c25792657d93fc88253e992cffc53acc444a429a
Jonathan Fernandes <jhfernan@mail.usf.edu>
2018-12-25 12:25:34 -0500

    Config setup and eslint tab error fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,74 +1,58 @@
-const pkg = require('./package')
+const pkg = require("./package");
 
 module.exports = {
-  mode: 'universal',
+	mode: "universal",
 
-  /*
-  ** Headers of the page
-  */
-  head: {
-    title: pkg.name,
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' }
-    ]
-  },
+	// Headers of the page
+	head: {
+		title: pkg.name,
+		meta: [
+			{ charset: "utf-8" },
+			{ name: "viewport", content: "width=device-width, initial-scale=1" },
+			{ hid: "description", name: "description", content: pkg.description }
+		],
+		link: [
+			// { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+			{
+				rel: "stylesheet",
+				href:
+					"https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons"
+			}
+		]
+	},
 
-  /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
+	// Axios module configuration
+	axios: {
+		// See https://github.com/nuxt-community/axios-module#options
+	},
 
-  /*
-  ** Global CSS
-  */
-  css: [
-    '~/assets/style/app.styl'
-  ],
+	// Global CSS
+	css: ["~/assets/style/app.styl"],
+	// Customize the progress-bar color
+	loading: { color: "#fff" },
 
-  /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-    '@/plugins/vuetify'
-  ],
+	// Nuxt.js modules
+	modules: [
+		// Doc: https://github.com/nuxt-community/axios-module#usage
+		"@nuxtjs/axios"
+	],
 
-  /*
-  ** Nuxt.js modules
-  */
-  modules: [
-    // Doc: https://github.com/nuxt-community/axios-module#usage
-    '@nuxtjs/axios'
-  ],
-  /*
-  ** Axios module configuration
-  */
-  axios: {
-    // See https://github.com/nuxt-community/axios-module#options
-  },
+	// Plugins to load before mounting the App
+	plugins: ["@/plugins/vuetify"],
 
-  /*
-  ** Build configuration
-  */
-  build: {
-    /*
-    ** You can extend webpack config here
-    */
-    extend(config, ctx) {
-      // Run ESLint on save
-      if (ctx.isDev && ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
-    }
-  }
-}
+	// Build configuration
+	build: {
+		// You can extend webpack config here
+		extend(config, ctx) {
+			// Run ESLint on save
+			if (ctx.isDev && ctx.isClient) {
+				config.module.rules.push({
+					enforce: "pre",
+					test: /\.(js|vue)$/,
+					loader: "eslint-loader",
+					exclude: /(node_modules)/
+				});
+			}
+		}
+	}
+};
