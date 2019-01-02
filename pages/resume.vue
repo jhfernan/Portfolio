46860d12ad6c94c683271643a0dd3a1e9fdf7c78
Jonathan Fernandes <jhfernan@mail.usf.edu>
2019-01-02 15:03:46 -0500

    Created resume page

---
+++ pages/resume.vue
@@ -0,0 +1,29 @@
+<template>
+	<v-container class="cb-container mt-5">
+		<div :key="i" v-for="(section, i) in resume">
+			<h3>{{ section.title }}</h3>
+			<markdown :anchorAttributes="{ target: '_blank', rel: 'nofollow' }" class="subheading">{{ section.body }}</markdown>
+		</div>
+	</v-container>
+</template>
+
+<script>
+export default {
+	asyncData ({ app, error }) {
+		return app.$axios.$get('/info/resume')
+		.then(res => {
+			return { resume: res }
+		})
+		.catch(err => { error({ statusCode: '404', message: 'Could not retrieve resume info' }) })
+	},
+	head () {
+		return {
+			title: 'Jonathan Fernandes | Resume',
+			meta: [
+				{ hid: 'description', name: 'description', content: 'Resume for Jonathan Fernandes' }
+			]
+		}
+	},
+	middleware: 'guest',
+}
+</script>
