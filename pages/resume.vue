eba775af0757f51066dd758f4fd321e396a4bbae
Jonathan Fernandes <jhfernan@mail.usf.edu>
2019-01-03 14:51:34 -0500

    Updated resume page and styles for markdown

--- pages/resume.vue
+++ pages/resume.vue
@@ -2,7 +2,7 @@
 	<v-container class="cb-container mt-5">
 		<div :key="i" v-for="(section, i) in resume">
 			<h2 class="mb-2" :class="i == 0 ? '' : 'mt-5'">{{ section.title }}</h2>
-			<markdown :anchorAttributes="{ target: '_blank', rel: 'nofollow' }" class="subheading">{{ section.body }}</markdown>
+			<markdown :anchorAttributes="{ target: '_blank', rel: 'nofollow' }" class="markdown">{{ section.body }}</markdown>
 		</div>
 	</v-container>
 </template>
